0932090d71d0371e15a20f00f57e6f48edd928a7
jonatasassis <assisjhonydalbello@gmail.com>
2024-04-30 13:25:29 -0300

    Aula 32.1
    Add conteudo Aula 32.1

---
+++ Assets/Scripts/Enemies/EnemyWalkable.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemy
+{
+    public class EnemyWalkable : EnemyBase
+    {
+        public GameObject[] wayPoints;
+        private int index = 0;
+        public float minDistance = 1;
+        public float speed ;
+
+        private void Update()
+        {
+            if (Vector3.Distance(transform.position, wayPoints[index].transform.position) < minDistance)
+            {
+                index++;
+
+                if(index>= wayPoints.Length)
+                {
+                    index = 0;
+                }
+                
+            }
+            transform.position = Vector3.MoveTowards(transform.position, wayPoints[index].transform.position, Time.deltaTime * speed);
+        }
+    }
+}
