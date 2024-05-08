65f8cb917fd8721fe0b20dfc4db3cad12d3e5b55
jonatasassis <assisjhonydalbello@gmail.com>
2024-05-08 16:05:03 -0300

    Aula 33.4
    Add conteudo Aula 33.4

---
+++ Assets/Scripts/CheckPoints/CheckPointBase.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointBase : MonoBehaviour
+{
+    public MeshRenderer meshRenderer;
+    public int key = 01;
+    private string checkPointKey= "CheckPointKey";
+    private bool checkPointActivated = false;
+    private void OnTriggerEnter(Collider col)
+    {
+        if (!checkPointActivated && col.transform.tag == "Player")
+        {
+            TurnOnCheckPoint();
+            CheckCheckPoint();
+        }
+    }
+
+    private void CheckCheckPoint()
+    {
+        SaveCheckPoint();
+    }
+
+    [NaughtyAttributes.Button]
+    private void TurnOffCheckPoint()
+    {
+        meshRenderer.material.SetColor("_EmissionColor", Color.grey);
+    }
+    [NaughtyAttributes.Button]
+    private void TurnOnCheckPoint()
+    {
+        meshRenderer.material.SetColor("_EmissionColor", Color.white);
+        SaveCheckPoint();
+        print("liguei o checkpoint "+key);
+    }
+
+    private void SaveCheckPoint()
+    {
+        if (PlayerPrefs.GetInt(checkPointKey, 0) > key)
+        {
+            PlayerPrefs.SetInt(checkPointKey, key);
+        }
+        checkPointActivated = true;
+    }
+}
