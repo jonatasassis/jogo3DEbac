bfa78adae89f46df89fb06c1cbae95d2d8d86bb9
jonatasassis <assisjhonydalbello@gmail.com>
2024-05-30 15:57:13 -0300

    Aula 36.4
    Add conteudo Aula 36.4

--- Assets/Scripts/DestructableItem/DestructableItemBase.cs
+++ Assets/Scripts/DestructableItem/DestructableItemBase.cs
@@ -6,6 +6,11 @@ using UnityEngine;
 public class DestructableItemBase : MonoBehaviour
 {
     public HealthBase healthBase;
+    public float shakeDuration = .1f;
+    public int shakeForce=1;
+    public int dropCoinsAmount = 10;
+    public GameObject coinPrefab;
+    public Transform dropPosition;
 
     private void OnValidate()
     {
@@ -24,5 +29,13 @@ public class DestructableItemBase : MonoBehaviour
     private void OnDamage(HealthBase h)
     {
         transform.DOShakeScale(.5f,Vector3.up,5);
+        DropCoin();
+    }
+
+    private void DropCoin()
+    {
+        var i = Instantiate(coinPrefab);
+        i.transform.position = dropPosition.position;
+        i.transform.DOScale(0, 1f).SetEase(Ease.OutBack).From();
     }
 }
